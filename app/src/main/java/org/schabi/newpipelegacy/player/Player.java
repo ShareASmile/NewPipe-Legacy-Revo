1dd439321f945b938ad017c211615d71ad38cc7a
ShareASmile <60492161+ShareASmile@users.noreply.github.com>
2024-06-23 17:47:49 +0530

    Update Player.java
    Co-Authored-By: Stypox <stypox@pm.me>

--- app/src/main/java/org/schabi/newpipelegacy/player/Player.java
+++ app/src/main/java/org/schabi/newpipelegacy/player/Player.java
@@ -1524,12 +1524,21 @@ public final class Player implements PlaybackListener, Listener {
 
         currentItem = item;
 
-        final int playQueueIndex = playQueue.indexOf(item);
-        final int playlistIndex = simpleExoPlayer.getCurrentMediaItemIndex();
-        final int playlistSize = simpleExoPlayer.getCurrentTimeline().getWindowCount();
-        final boolean removeThumbnailBeforeSync = currentItem == null
-                || currentItem.getServiceId() != item.getServiceId()
-                || !currentItem.getUrl().equals(item.getUrl());
+        if (playQueueIndex != playQueue.getIndex()) {
+            // wrong window (this should be impossible, as this method is called with
+            // `item=playQueue.getItem()`, so the index of that item must be equal to `getIndex()`)
+            Log.e(TAG, "Playback - Play Queue may be not in sync: item index=["
+                    + playQueueIndex + "], " + "queue index=[" + playQueue.getIndex() + "]");
+
+        } else if ((playlistSize > 0 && playQueueIndex >= playlistSize) || playQueueIndex < 0) {
+            // the queue and the player's timeline are not in sync, since the play queue index
+            // points outside of the timeline
+            Log.e(TAG, "Playback - Trying to seek to invalid index=[" + playQueueIndex
+                    + "] with playlist length=[" + playlistSize + "]");
+
+        } else if (wasBlocked || playlistIndex != playQueueIndex || !isPlaying()) {
+            // either the player needs to be unblocked, or the play queue index has just been
+            // changed and needs to be synchronized, or the player is not playing
             if (DEBUG) {
                 Log.d(TAG, "Playback - Rewinding to correct index=[" + playQueueIndex + "], "
                         + "from=[" + playlistIndex + "], size=[" + playlistSize + "].");
